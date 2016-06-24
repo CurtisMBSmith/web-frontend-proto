bc860df995405caaf5db64a4903c75d20cdd9279
Curtis Smith <Curtis.MBSmith@gmail.com>
2016-06-23 21:45:50 -0300

    Removed old tutorial files.

--- src/index.js
+++ src/index.js
@@ -5,24 +5,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import app from './reducers';
-// import './styles.scss';
-
-if (document.querySelectorAll('a').length) {
-  require.ensure([], () => {
-    const Button = require('./components/Button').default;
-    const button = new Button('google.com');
-
-    button.render('a');
-  });
-}
-
-if (document.querySelectorAll('h1').length) {
-  require.ensure([], () => {
-    const Header = require('./components/Header').default;
-
-    new Header().render('h1');
-  });
-}
 
 let store = createStore(app);
 
