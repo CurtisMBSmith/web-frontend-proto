eb01bcd02fd28d25952af023ef84a9d39f69ac90
Curtis Smith <Curtis.MBSmith@gmail.com>
2016-07-04 20:51:52 -0300

    Added form validation on the LoginForm.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,29 @@
-import React from 'react';
-// import { connect } from 'react-redux';
+import React, { Component } from 'react';
 
-const LoginForm = ({ onLogIn }) => {
-  let email, password;
-  return (
-    <div className="loginForm" >
-      <input type="email" placeholder="Enter email address" ref={node => {
-        email = node
-      }} required/>
-      <input type="password" placeholder="hunter2" ref={node => {
-        password = node
-      }} required/>
-      <button type="button" onClick={e => {
-      if (!email.value.trim()) {
-        return
-      }
-      onLogIn("authToken", email.value);
-    }}>Log In</button>
-      <button type="button" >Forgot Password?</button>
-   </div>
-  );
-};
+export default class LoginForm extends Component {
+  render() {
+    let email, password;
+    return (
+      <form className="loginForm" >
+        <input type="email" placeholder="Enter email address" ref={node => {
+          email = node
+        }} required/>
+        <input type="password" placeholder="hunter2" ref={node => {
+          password = node
+        }} required/>
+        <button type="submit" onClick={e => {
+        if (!email.value.trim()) {
+          return
+        }
+        if (!email.validity.valid || !password.validity.valid) {
+          return;
+        }
+        this.props.onLogIn("authToken", email.value);
+      }}>Log In</button>
+        <button type="button" >Forgot Password?</button>
 
-export default LoginForm;
+     </form>
+    );
+  };
+
+}
